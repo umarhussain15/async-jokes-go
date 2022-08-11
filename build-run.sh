235646094eee9114b7d3f23a1ac42b9966282d5e
Umar Hussain <umarhussain.work@gmail.com>
2022-08-11 12:56:07 +0200

    feat: create docker network to share
    A new docker network is created if missing, to allow application
    container connect to RabbitMQ server by container name.

--- build-run.sh
+++ build-run.sh
@@ -2,4 +2,6 @@
 
 docker build -t jokes-app .
 
-docker run -it --rm --net=host --name jokes-app jokes-app --uri "amqp://guest:guest@localhost:5672/"
+docker network create rabbit-mq-demo 2>/dev/null
+
+docker run -it --rm --net=rabbit-mq-demo --name jokes-app jokes-app --uri "amqp://guest:guest@rabbitmq:5672/"
--- run-rabbit-mq.sh
+++ run-rabbit-mq.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -it --rm --net=host  --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.10-management
+docker network create rabbit-mq-demo  2>/dev/null
+
+docker run -it --rm --net=rabbit-mq-demo  --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.10-management
